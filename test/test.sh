c2a72fbd71967ef2ff7ccfd03d59d3ae3aae94b3
Nick Santos <nicholas.j.santos@gmail.com>
2020-04-02 18:30:39 -0400

    Add a circleci test for the example html guide

---
+++ test/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+# cd to the root of the repo.
+cd $(dirname $(dirname $0))
+
+echo "Testing 0-base"
+tilt ci --file 0-base/Tiltfile
+tilt down --file 0-base/Tiltfile
+
+echo "Testing 1-measured"
+tilt ci --file 1-measured/Tiltfile
+tilt down --file 1-measured/Tiltfile
+
+echo "Testing 2-recommended"
+tilt ci --file 2-recommended/Tiltfile
+tilt down --file 2-recommended/Tiltfile
