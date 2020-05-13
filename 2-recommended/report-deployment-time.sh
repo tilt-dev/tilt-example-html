a30fc917e8d66c183e81db16600cd9ee93d359e3
Nick Santos <nicholas.j.santos@gmail.com>
2020-05-13 11:58:11 -0400

    handle space issues when printing time (#7)

--- 2-recommended/report-deployment-time.sh
+++ 2-recommended/report-deployment-time.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 DEPLOY_TIME_SEC=$(date +%s)
-DEPLOY_DURATION_SEC=$(expr $DEPLOY_TIME_SEC - $(cat start-time.txt))
+DEPLOY_DURATION_SEC=$(expr $DEPLOY_TIME_SEC - $(cat start-time.txt | tr -d "[:space:]"))
 echo "Deploy time in seconds: $DEPLOY_DURATION_SEC"
 sed -i "s/^.*Deploy.*$/<div>Deploy time: $DEPLOY_DURATION_SEC sec/g" index.html
